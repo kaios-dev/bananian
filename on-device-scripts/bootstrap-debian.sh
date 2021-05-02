fe1a6efe883a5a0cbd9787fbd1c85f19501934a8
Affe Null <affenull2345@gmail.com>
2021-05-02 13:33:16 +0200

    Add the default user to the audio and ofono groups

--- on-device-scripts/bootstrap-debian.sh
+++ on-device-scripts/bootstrap-debian.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 echo "[on device] ==>> Bootstrapping debian"
-/data/run-in-debian.sh $@ 'debootstrap/debootstrap --second-stage; cd /var/cache && dpkg -i *.deb && rm -f *.deb && adduser --gecos "Bananian User" --disabled-password user && adduser user sudo && (echo user:bananian | chpasswd user)'
+/data/run-in-debian.sh $@ 'debootstrap/debootstrap --second-stage; cd /var/cache && dpkg -i *.deb && rm -f *.deb && adduser --gecos "Bananian User" --disabled-password user && adduser user sudo && addgroup --system ofono && adduser user ofono && adduser user audio && (echo user:bananian | chpasswd user)'
